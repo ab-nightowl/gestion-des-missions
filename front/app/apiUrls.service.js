ec487785f1640d55657911718e6f8a86951c97a0
Damien <dguironnet@gmail.com>
2017-09-06 17:23:01 +0200

    merging conflicts #3 between #M022, #M001, #M002

--- front/app/apiUrls.service.js
+++ front/app/apiUrls.service.js
@@ -1,8 +1,7 @@
 const apiUrls = {
-<<<<<<< HEAD
     base: `${API_URL}`,
     utilisateurs: `${API_URL}/utilisateurs`,
-    mission : `${API_URL}/listMission`
+    mission : `${API_URL}/listMission`,
     naturesMission: `${API_URL}/naturesmission`,
     naturesFrais : `${API_URL}/listNaturesFrais`,
     villes: `${API_URL}/villes`,
